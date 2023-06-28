4fc9a861e93c60580c4f596f9b858ac53e315b6f
Elena Lungoci <simona.lungoci@yahoo.com>
2023-06-28 18:14:59 -0400

    different alveoli paths for enemy

---
+++ Corona Tower Defense/Assets/Scripts/EnemyMovementScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+
+    int lungs_path;
+    int alveoli_path;
+
+    void Start(){
+        alveoli_path = Random.Range(1,7);
+        Debug.Log(alveoli_path);
+    }
+    
+
+    public void GoToAlveoliAnimation(){
+        //animator.SetBool("finishedLungsPath", true);
+        animator.SetInteger("alveoli_path", alveoli_path);
+    }
+
+    public void ReachedTheEnd(){
+        // increase humidity
+
+        Destroy(gameObject);
+    }
+
+   
+}
